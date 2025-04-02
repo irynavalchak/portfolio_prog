b344fabd745574fb18591b1595c1806dddf608d9
Iryna <iravalchak@gmail.com>
2025-04-02 17:49:50 +0300

    change commit

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 let certification = document.getElementById("certification");
+let about = document.getElementById("about");
+let skills = document.getElementById("skills");
+let experience = document.getElementById("experience");
+let education = document.getElementById("education");
+let contact = document.getElementById("contact");
 let certificationLink = document.querySelector('a[href="/certification"]');
+let skillsLink = document.querySelector('a[href="/skills"]');
+let experienceLink = document.querySelector('a[href="/experience"]');
+let educationLink = document.querySelector('a[href="/education"]');
+let contactLink = document.querySelector('a[href="/contact"]');
+let aboutLink = document.querySelector('a[href="/about"]');
 
 certificationLink.addEventListener('click', e => {
     e.preventDefault();
     certification.scrollIntoView({ behavior: 'smooth' });
 })
+
+
+skillsLink.addEventListener('click', e => {
+    e.preventDefault();
+    skills.scrollIntoView({ behavior: 'smooth' });
+})
+
+experienceLink.addEventListener('click', e => {
+    e.preventDefault();
+    experience.scrollIntoView({ behavior: 'smooth' });
+})
+
+educationLink.addEventListener('click', e => {
+    e.preventDefault();
+    education.scrollIntoView({ behavior: 'smooth' });
+})
+
+contactLink.addEventListener('click', e => {
+    e.preventDefault();
+    contact.scrollIntoView({ behavior: 'smooth' });
+})
+
+aboutLink.addEventListener('click', e => {
+    e.preventDefault();
+    about.scrollIntoView({ behavior: 'smooth' });
+})
+
+
